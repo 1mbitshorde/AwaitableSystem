bdd78ebd511e3f2e2e60d45c2606a5796e207427
Hyago Oliveira <hyagogow@gmail.com>
2025-08-24 09:05:08 -0300

    Add WaitUntilAsync function

---
+++ Runtime/AwaitableUtility.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace ActionCode.AwaitableSystem
+{
+    /// <summary>
+    /// Utility class for <see cref="Awaitable"/>.
+    /// </summary>
+    public static class AwaitableUtility
+    {
+        /// <summary>
+        /// Waits asynchronously until the given condition is true.
+        /// </summary>
+        /// <param name="condition">A boolean condition.</param>
+        /// <returns>An asynchronously operation.</returns>
+        public static async Awaitable WaitUntilAsync(Func<bool> condition)
+        {
+            while (!condition()) await Awaitable.NextFrameAsync();
+        }
+    }
+}
